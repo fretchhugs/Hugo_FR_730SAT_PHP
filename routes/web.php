16e40cc6a97100cfa372eb366c94f57a1bd6d230
Fretch Hugo <fretchhiramis@gmail.com>
2024-09-24 15:41:40 +0800

    0914_Video2_Create-A-Route-And-Load-A-View

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/About', function () {
+    return view('about');
+});
