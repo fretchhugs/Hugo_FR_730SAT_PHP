b658140e376f8278a41eb49ca5cb05eb5b232c37
Fretch Hugo <fretchhiramis@gmail.com>
2024-10-01 01:25:14 +0800

    0921_Video8_Introduction-to-Migrations

---
+++ storage/framework/views/2f781f5e3d72446402037de5b2ea38b6.php
@@ -0,0 +1,36 @@
+<?php if (isset($component)) { $__componentOriginal23a33f287873b564aaf305a1526eada4 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal23a33f287873b564aaf305a1526eada4 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.layout','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+ <?php $__env->slot('heading', null, []); ?> 
+       Job
+     <?php $__env->endSlot(); ?>
+
+    <ul>
+    <?php $__currentLoopData = $jobs; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $job): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <li>
+        <a href="/jobs/<?php echo e($job['id']); ?>" class="text-blue-500 hover: underline ">
+
+  
+        <strong><?php echo e($job['title']); ?>:</strong> Pays <?php echo e($job['salary']); ?> per year.</li>
+        </a> 
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </ul>
+
+
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal23a33f287873b564aaf305a1526eada4)): ?>
+<?php $attributes = $__attributesOriginal23a33f287873b564aaf305a1526eada4; ?>
+<?php unset($__attributesOriginal23a33f287873b564aaf305a1526eada4); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal23a33f287873b564aaf305a1526eada4)): ?>
+<?php $component = $__componentOriginal23a33f287873b564aaf305a1526eada4; ?>
+<?php unset($__componentOriginal23a33f287873b564aaf305a1526eada4); ?>
+<?php endif; ?><?php /**PATH C:\Users\Lenovo - i5 10th Gen\Desktop\ELPHP\babySteps\resources\views/jobs.blade.php ENDPATH**/ ?>
