740dc4fe4fb6b2df0fdc6c32c30a2dfb371c798c
fretchhugs <fretchhiramis@gmail.com>
2024-10-01 20:32:18 +0800

    0921_Vide12_Pivot-Tables-And-BelongsToMany-Relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -14,4 +14,10 @@ class Job extends Model {
     {
         return $this->belongsTo(Employer::class);
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
+
+    }
 }
