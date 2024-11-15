3a667b6263521c1c2586fb97c8aa020e5da7a5c3
fretchhugs <fretchhiramis@gmail.com>
2024-11-15 23:08:36 +0800

    11-16_Video30_Wrap-Up

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Tag;
+class TagController extends Controller
+{
+    public function __invoke(Tag $tag)
+    {
+        return view('results', ['jobs' => $tag->jobs->load(['employer', 'tags'])]);
+    }
+}
--- database/migrations/2024_11_14_152050_create_jobs_table.php
+++ database/migrations/2024_11_14_152050_create_jobs_table.php
@@ -1,8 +1,10 @@
 <?php
+
 use App\Models\Employer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 return new class extends Migration
 {
     /**
@@ -12,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Employer::class);
+            $table->foreignIdFor(Employer::class)->constrained()->cascadeOnDelete();
             $table->string('title');
             $table->string('salary');
             $table->string('location');
@@ -22,6 +24,7 @@ return new class extends Migration
             $table->timestamps();
         });
     }
+
     /**
      * Reverse the migrations.
      */
